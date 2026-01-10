138c87f0e086d91f156f78efff3efbd64d1031db
Andy Due <andy061099@gmail.com>
2026-01-10 16:20:30 -0500

    commit id-contenedorDto

--- Models/CostoContenedorCreateDto.cs
+++ Models/CostoContenedorCreateDto.cs
@@ -3,7 +3,7 @@ namespace ImportadoraApi.Models
 {
     public class CostoContenedorCreateDto
     {
-        public Guid ContenedorDetalleId { get; set; }
+        public Guid ContenedorId { get; set; }
         public TipoMoneda Tipo { get; set; }
         public decimal Monto { get; set; }
         public string? Observaciones { get; set; }
